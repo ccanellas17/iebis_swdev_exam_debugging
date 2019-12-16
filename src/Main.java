1e661eaa92d07a179f5e46830191447cb06cfe38
Carlos Cañellas <42964716+ccanellas17@users.noreply.github.com>
2019-12-16 12:41:19 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
+package com.company;
+
 import java.util.Random;
 
 @SuppressWarnings("ALL")
 public class Main {
 
+
     public static void main(String[] args) {
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/");
 
         Random random = new Random();
         StringBuffer word = null;
+        int i = 0;
+        int randomNum = random.nextInt(2) + 1;
 
-        switch (random.nextInt(2)) {
+        switch (randomNum) {
             case 0:
-                word = new StringBuffer('Y');
+                word = new StringBuffer("Y");
+                break;
             case 1:
-                word = new StringBuffer('F');
+                word = new StringBuffer("F");
+                break;
             case 2:
-                word = new StringBuffer('T');
+                word = new StringBuffer("T");
+                break;
         }
 
         word.append('o');
